991251bff35a2b637aa1bfd0c29c2ff1aa70900c
DanielSeuChoi <choidanosa@gmail.com>
2023-01-18 17:14:01 -0800

    edited out h2 solar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
         <div className='container mt-4'>
             <div className="card shadow">
                 <div className="card-body">
-                    <h1>Solar</h1>
                     <Userhform />
                 </div>
             </div>
